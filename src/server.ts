e998b2fe276a2e2a6779b88f9e2a218e0471f9d3
Jonny Burger <jonathanburger11@gmail.com>
2019-05-06 15:33:42 +0200

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import mongo from './mongo';
 
 const router = express.Router();
 
+router.use(cors());
 router.get('/', async (request, response) => {
 	const db = await mongo();
 	const runs = await db.runs.find().toArray();
