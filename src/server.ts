d00b288a6fc7363373cccaea2d52a69d008c0289
Jonny Burger <jonathanburger11@gmail.com>
2019-05-09 15:04:39 +0200

    pagination

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,13 @@ const router = express.Router();
 
 router.use(cors());
 router.get('/', async (request, response) => {
+	const offset = Number(request.query.offset || 0);
 	const db = await mongo();
-	const runs = await db.runs.find().toArray();
+	const runs = await db.runs
+		.find()
+		.skip(offset)
+		.limit(100)
+		.toArray();
 	const filled = fillUntracked(runs);
 	response.json(filled);
 });
