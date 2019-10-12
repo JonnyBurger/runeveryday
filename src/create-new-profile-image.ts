8d6810563cbbdebfd92f9a786a5209cb62dd7e95
Jonny Burger <jonathanburger11@gmail.com>
2019-10-12 22:01:50 +0200

    automatic dark mode

--- src/create-new-profile-image.ts
+++ src/create-new-profile-image.ts
@@ -1,15 +1,21 @@
 import Jimp = require('jimp');
 
 export default async (): Promise<string> => {
+	const hoursInZurich = new Date(Date.now() + 60 * 1000 * 2).getUTCHours();
+	const darkMode = hoursInZurich >= 19 || hoursInZurich < 7;
 	const bg = await Jimp.read('https://gradientjoy.com/400x400');
-	const white = await Jimp.read('./assets/white.png');
+	const background = await Jimp.read(
+		darkMode ? './assets/black.png' : './assets/white.png'
+	);
 
-	const lightBg = await bg.composite(white, 0, 0, {
+	const lightBg = await bg.composite(background, 0, 0, {
 		opacityDest: 1,
-		opacitySource: 0.7,
+		opacitySource: darkMode ? 0.4 : 0.7,
 		mode: Jimp.BLEND_SOURCE_OVER
 	});
-	const fg = await Jimp.read('./assets/jonny400.png');
+	const fg = await Jimp.read(
+		darkMode ? './assets/jonny-dark.png' : './assets/jonny-light.png'
+	);
 	const combined = await lightBg.composite(fg, 0, 0);
 	return combined.getBase64Async('image/png');
 };
