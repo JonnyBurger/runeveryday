52644889ede26d277492fff7dd8744b00c8a4480
Jonny Burger <jonathanburger11@gmail.com>
2019-11-15 21:42:48 +0100

    workflow updates

--- src/update.ts
+++ src/update.ts
@@ -1,18 +1,10 @@
 import lookForNewRuns from './look-for-new-runs';
-import updateTwitterBio from './update-twitter-bio';
 
 lookForNewRuns()
-	.then(
-		(highestNumber: number): Promise<void> => updateTwitterBio(highestNumber)
-	)
-	.then(
-		(): void => {
-			console.log('done');
-			process.exit(0);
-		}
-	)
-	.catch(
-		(err): void => {
-			console.log(err);
-		}
-	);
+  .then((): void => {
+    console.log('done');
+    process.exit(0);
+  })
+  .catch((err): void => {
+    console.log(err);
+  });
