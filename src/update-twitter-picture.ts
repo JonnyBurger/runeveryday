85fb099b886200a799fdc1347b729c21211ceb4f
Jonny Burger <jonathanburger11@gmail.com>
2020-01-23 16:26:37 +0100

    hi webzurich :wave:

--- src/update-twitter-picture.ts
+++ src/update-twitter-picture.ts
@@ -1,7 +1,8 @@
 import dotenv from 'dotenv';
 import Twit from 'twit';
 import xns from 'xns';
-import createNewProfileImage from './create-new-profile-image';
+
+import Jimp = require('jimp');
 
 dotenv.config();
 
@@ -14,7 +15,29 @@ const T = new Twit({
 
 export default xns(
 	async (): Promise<void> => {
-		const image = await createNewProfileImage();
+		const hoursInZurich = parseInt(
+			new Intl.DateTimeFormat('en', {
+				timeZone: 'Europe/Zurich',
+				hour: '2-digit',
+				hour12: false
+			}).format(new Date())
+		);
+		const darkMode = hoursInZurich >= 19 || hoursInZurich < 6.9;
+		const bg = await Jimp.read('https://gradientjoy.com/400x400');
+		const background = await Jimp.read(
+			darkMode ? './assets/black.png' : './assets/white.png'
+		);
+
+		const lightBg = bg.composite(background, 0, 0, {
+			opacityDest: 1,
+			opacitySource: darkMode ? 0.4 : 0.7,
+			mode: Jimp.BLEND_SOURCE_OVER
+		});
+		const fg = await Jimp.read(
+			darkMode ? './assets/jonny-dark.png' : './assets/jonny-light.png'
+		);
+		const combined = lightBg.composite(fg, 0, 0);
+		const image = await combined.getBase64Async('image/png');
 		await T.post('account/update_profile_image', {
 			// @ts-ignore
 			image: image.replace('data:image/png;base64,', '')
