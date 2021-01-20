4785189d338c28cb3b52e9b901d8cdddafa5b19c
Jonny Burger <jonathanburger11@gmail.com>
2021-01-20 21:55:58 +0100

    limit to end

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import {flatten, uniq} from 'lodash';
+import { flatten, uniq } from 'lodash';
 import mongo from './mongo';
 import fillUntracked from './fill-untracked';
 import getDay from './get-day';
@@ -17,16 +17,12 @@ router.get(
 		const day = getDay(new Date()) - offset;
 		const runs = await db.runs
 			.find({
-				day: {$lte: day, $gt: day - 100}
+				day: { $lte: new Date('2020-12-17').getTime(), $gt: day - 100 },
 			})
 			.toArray();
 		const filled = fillUntracked(runs, offset, 100);
-		const one = await db.runs
-			.find({})
-			.sort({day: -1})
-			.limit(1)
-			.toArray();
-		response.json({runs: filled, total: one ? one[0].day : 0});
+		const one = await db.runs.find({}).sort({ day: -1 }).limit(1).toArray();
+		response.json({ runs: filled, total: one ? one[0].day : 0 });
 	}
 );
 
@@ -37,7 +33,9 @@ router.get(
 		const overrideCountries = flatten(
 			(overrides
 				.map((o): string | null | undefined => o.country)
-				.filter(Boolean) as string[]).map((c: string): string[] => c.split(','))
+				.filter(Boolean) as string[]).map((c: string): string[] =>
+				c.split(',')
+			)
 		) as string[];
 		const dbCountries = flatten(
 			(await db.runs.distinct('country', {}))
@@ -45,7 +43,7 @@ router.get(
 				.map((c: string): string[] => c.split(','))
 		);
 		response.json({
-			countries: uniq([...overrideCountries, ...dbCountries])
+			countries: uniq([...overrideCountries, ...dbCountries]),
 		});
 	}
 );
@@ -59,13 +57,13 @@ router.get(
 				{
 					$group: {
 						_id: null,
-						distance: {$avg: '$distance'}
-					}
-				}
+						distance: { $avg: '$distance' },
+					},
+				},
 			])
 			.toArray();
 		response.json({
-			distance: result[0].distance
+			distance: result[0].distance,
 		});
 	}
 );
